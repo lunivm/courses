e4addcfe78d686120cdf4a2c017a58ebef476a17
Mykola Luniv <lunivm@yahoo.com>
2025-08-21 00:14:58 +0300

    meta-react-advanced: step 3

--- meta-react-advanced/src/components/Card.js
+++ meta-react-advanced/src/components/Card.js
@@ -4,10 +4,22 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
 const Card = ({ title, description, imageSrc }) => {
-  // Implement the UI for the Card component according to the instructions.
-  // You should be able to implement the component with the elements imported above.
-  // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+  return (
+    <VStack bg="white" rounded="lg" align="start">
+      <Image src={imageSrc} alt={title} rounded="lg"/>
+      <VStack bg="white" rounded="lg" align="start" p={4}>
+        <Heading as="h3" size="sm" color="black">
+          {title}
+        </Heading>
+        <Text color="gray.600" py={4}>{description}</Text>
+
+        <HStack pt={2} spacing={2} color="gray.800" cursor="pointer">
+          <Text fontWeight="bold">See more</Text>
+          <FontAwesomeIcon icon={faArrowRight} size="sm" />
+        </HStack>
+      </VStack>
+    </VStack>
+  );
 };
 
 export default Card;
